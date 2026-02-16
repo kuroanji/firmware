984216f298723e818c384745e3a2d300b2ba0b9f
kuroanji <33121731+kuroanji@users.noreply.github.com>
2026-02-16 13:45:04 +0400

    Update AppletFont.h

--- src/graphics/niche/InkHUD/AppletFont.h
+++ src/graphics/niche/InkHUD/AppletFont.h
@@ -13,6 +13,7 @@
 #include "configuration.h"
 
 #include <GFX.h> // GFXRoot drawing lib
+#include "graphics/niche/Fonts/CJK/CJKFont.h"
 
 namespace NicheGraphics::InkHUD
 {
@@ -27,10 +28,13 @@ class AppletFont
         WINDOWS_1251,
         WINDOWS_1252,
         WINDOWS_1253,
+        JAPANESE,
     };
 
     AppletFont();
     AppletFont(const GFXfont &adafruitGFXFont, Encoding encoding = ASCII, int8_t paddingTop = 0, int8_t paddingBottom = 0);
+    AppletFont(const GFXfont &adafruitGFXFont, Encoding encoding, int8_t paddingTop, int8_t paddingBottom,
+               const NicheGraphics::CJKFont *cjkFont, float cjkScale = 1.0f);
 
     uint8_t lineHeight();
     uint8_t heightAboveCursor();
@@ -40,6 +44,8 @@ class AppletFont
     std::string decodeUTF8(std::string encoded);
 
     const GFXfont *gfxFont = NULL; // Default value: in-built AdafruitGFX font
+    const NicheGraphics::CJKFont *cjkFont = nullptr;
+    float cjkScale = 1.0f;
 
   private:
     uint32_t toUtf32(std::string utf8);
@@ -92,5 +98,11 @@ class AppletFont
 #define FREESANS_12PT_WIN1253 InkHUD::AppletFont(FreeSans12pt_Win1253, InkHUD::AppletFont::WINDOWS_1253, -3, 1)
 #define FREESANS_9PT_WIN1253 InkHUD::AppletFont(FreeSans9pt_Win1253, InkHUD::AppletFont::WINDOWS_1253, -2, -1)
 #define FREESANS_6PT_WIN1253 InkHUD::AppletFont(FreeSans6pt_Win1253, InkHUD::AppletFont::WINDOWS_1253, -1, -2)
+
+// Japanese (CJK) - uses Win1251 base font with CJK bitmap overlay
+#include "graphics/niche/Fonts/CJK/CJKFont_DotGothic18px.h"
+#define FREESANS_12PT_JP InkHUD::AppletFont(FreeSans12pt_Win1251, InkHUD::AppletFont::JAPANESE, -3, 1, &CJKFontDotGothic18px, 1.0f)
+#define FREESANS_9PT_JP InkHUD::AppletFont(FreeSans9pt_Win1251, InkHUD::AppletFont::JAPANESE, -2, -1, &CJKFontDotGothic18px, 0.72f)
+#define FREESANS_6PT_JP InkHUD::AppletFont(FreeSans6pt_Win1251, InkHUD::AppletFont::JAPANESE, -1, -2, &CJKFontDotGothic18px, 0.67f)
 
 #endif
